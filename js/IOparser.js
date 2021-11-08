a78cee95ee4645edcdfe81b9e85aba845b2f9cfc
zeekgwang <zeekgwang@gmail.com>
2021-11-09 00:00:23 +0900

    feat : change color

--- js/IOparser.js
+++ js/IOparser.js
@@ -8,32 +8,81 @@ const ctx = canvas.getContext('2d')
 
 document.getElementById("run").onclick = initialize
 document.getElementById("resetLotColor").onclick = resetLotColor
+document.getElementById("resetPPIDColor").onclick = resetPPIDColor
+document.getElementById("resetSchedPriColor").onclick = resetSchedPriColor
+
 
 var schedule;
 
 var eqpSet = {}
 var chamberHeight = {}
 
+var colorType = 'LOT'
+var schedPriColor = {
+    '0run': 'red',
+    '0wait': 'orange',
+    '1run': 'yellow',
+    '1wait': 'green',
+    '2run': 'blue',
+    '2wait': 'darkblue',
+    '3run': 'purple'
+}
+var ppidColor = {}
+var lotColor = {}
+
 
 function getRandomColor() {
     return '#' + Math.round(Math.random() * 0xffffff).toString(16)
 }
-
-var lotColor = {}
 
 
 function resetLotColor() {
     Object.keys(lotColor).forEach(function (key) {
         lotColor[key] = getRandomColor()
     });
+    colorType = 'LOT'
     draw(schedule)
 }
 
+function resetPPIDColor() {
+    Object.keys(ppidColor).forEach(function (key) {
+        ppidColor[key] = getRandomColor()
+    });
+    colorType = 'PPID'
+    draw(schedule)
+}
+
+function resetSchedPriColor() {
+    colorType = 'SCHEDPRI'
+    draw(schedule)
+}
 
 function drawRect(x, y, width, height, color) {
-    console.log(x, y, width, height)
     ctx.fillStyle = color
-    ctx.fillRect(x, y, width, height     );
+    ctx.fillRect(x, y, width, height);
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 1;
+    ctx.strokeRect(x, y, width, height);
+}
+
+function drawEqp(eqpSet) {
+    Object.keys(eqpSet).forEach(function (key) {
+        drawRect(0, eqpSet[key] * 7 * BLOCK_HEIGHT + BLOCK_HEIGHT, LEFT_MARGIN - BLOCK_HEIGHT * 3, BLOCK_HEIGHT * 6, 'lightgrey')
+        ctx.fillStyle = 'black'
+        ctx.textAlign = 'center'
+        ctx.textBaseline = 'middle'
+        ctx.font = 'bold ' + BLOCK_HEIGHT / 1.5 + 'px serif';
+        ctx.fillText(key, (LEFT_MARGIN - BLOCK_HEIGHT * 3) / 2, eqpSet[key] * 7 * BLOCK_HEIGHT + BLOCK_HEIGHT * 4)
+
+        for (var i = 1; i <= 6; i++) {
+            drawRect(LEFT_MARGIN - BLOCK_HEIGHT * 3, eqpSet[key] * 7 * BLOCK_HEIGHT + BLOCK_HEIGHT * i, BLOCK_HEIGHT, BLOCK_HEIGHT, 'darkgrey')
+            ctx.fillStyle = 'black'
+            ctx.textAlign = 'center'
+            ctx.textBaseline = 'middle'
+            ctx.font = 'bold ' + BLOCK_HEIGHT / 1.5 + 'px serif';
+            ctx.fillText(i, LEFT_MARGIN - BLOCK_HEIGHT * 2.5, eqpSet[key] * 7 * BLOCK_HEIGHT + BLOCK_HEIGHT * (i + 0.5))
+        }
+    });
 }
 
 
@@ -47,7 +96,8 @@ function draw(schedule) {
         var eqp = schedule[i][0]
         var chamber = schedule[i][1]
         var lot = schedule[i][2]
-        var startTime = schedule[i][3]
+        var ppid = schedule[i][3]
+        var startTime = schedule[i][5]
 
         if (minTime > startTime) {
             minTime = startTime
@@ -58,44 +108,43 @@ function draw(schedule) {
             eqpIdx = eqpIdx + 1
         }
 
-        if (chamberHeight[chamber] === undefined){
+        if (chamberHeight[chamber] === undefined) {
             chamberHeight[chamber] = eqpSet[eqp] * 7 * BLOCK_HEIGHT + parseInt(chamber.split('-')[1]) * BLOCK_HEIGHT
         }
 
         if (lotColor[lot] === undefined) {
             lotColor[lot] = getRandomColor()
         }
+
+        if (ppidColor[ppid] === undefined) {
+            ppidColor[ppid] = getRandomColor()
+        }
     }
 
     minTime = moment(minTime)
 
-    Object.keys(eqpSet).forEach(function (key) {
-        drawRect(0, eqpSet[key] * 7 * BLOCK_HEIGHT + BLOCK_HEIGHT, LEFT_MARGIN - BLOCK_HEIGHT * 3, BLOCK_HEIGHT * 6, 'lightgrey')
-        ctx.fillStyle = 'black'
-        ctx.textAlign = 'center'
-        ctx.textBaseline = 'middle'
-        ctx.font = 'bold ' + BLOCK_HEIGHT / 1.5 + 'px serif';
-        ctx.fillText(key, (LEFT_MARGIN - BLOCK_HEIGHT * 3) / 2, eqpSet[key] * 7 * BLOCK_HEIGHT + BLOCK_HEIGHT * 4)
-
-        for(var i = 1 ; i <= 6 ; i++){
-            drawRect(LEFT_MARGIN - BLOCK_HEIGHT * 3, eqpSet[key] * 7 * BLOCK_HEIGHT + BLOCK_HEIGHT * i, BLOCK_HEIGHT, BLOCK_HEIGHT, 'darkgrey')
-            ctx.fillStyle = 'black'
-            ctx.textAlign = 'center'
-            ctx.textBaseline = 'middle'
-            ctx.font = 'bold ' + BLOCK_HEIGHT / 1.5 + 'px serif';
-            ctx.fillText(i, LEFT_MARGIN - BLOCK_HEIGHT * 2.5, eqpSet[key] * 7 * BLOCK_HEIGHT + BLOCK_HEIGHT * (i + 0.5))
-        }
-    });
+    drawEqp(eqpSet)
 
     for (var i = 0; i < schedule.length; i++) {
         var chamber = schedule[i][1]
         var lot = schedule[i][2]
-        var startTime = schedule[i][3]
+        var ppid = schedule[i][3]
+        var schedPri = schedule[i][4]
+        var startTime = schedule[i][5]
         startTime = moment(startTime).diff(minTime, 'seconds')
-        var endTime = schedule[i][4]
+        var endTime = schedule[i][6]
         endTime = moment(endTime).diff(minTime, 'seconds')
 
-        drawRect(LEFT_MARGIN + startTime * BLOCK_WIDTH, chamberHeight[chamber], (endTime - startTime + 1) * BLOCK_WIDTH, BLOCK_HEIGHT, lotColor[lot])
+        var color = lotColor[lot]
+        if (colorType === 'LOT') {
+            color = lotColor[lot]
+        } else if (colorType === 'PPID') {
+            color = ppidColor[ppid]
+        } else if (colorType === 'SCHEDPRI') {
+            color = schedPriColor[schedPri]
+        }
+
+        drawRect(LEFT_MARGIN + startTime * BLOCK_WIDTH, chamberHeight[chamber], (endTime - startTime + 1) * BLOCK_WIDTH, BLOCK_HEIGHT, color)
     }
 }
 
